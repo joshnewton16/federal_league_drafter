aa9941cfe0f9d852278ea9016e05e6ca0c2b7384
Josh Newton <joshnewton16@gmail.com>
2025-04-11 11:56:32 -0400

    Final Deployment code

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -29,6 +30,17 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './public/index.html'
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { 
+          from: 'public', 
+          to: '',
+          globOptions: {
+            ignore: ['**/index.html'] // Exclude index.html from being copied
+          }
+        },
+      ],
     })
   ],
   devServer: {
